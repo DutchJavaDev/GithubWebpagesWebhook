426e5848b43a3232f67b696cbc616aaef8b84b07
Boris Mulder <bmulder@cyclomedia.com>
2024-10-07 20:59:51 +0200

    rename function name remove unused stuff prepare for actual development

--- GithubWebpagesWebhook/Function1.cs
+++ GithubWebpagesWebhook/Function1.cs
@@ -12,24 +12,14 @@ namespace GithubWebpagesWebhook
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("GithubWebHook")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult("GithubWebHook test");
         }
     }
 }
