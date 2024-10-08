1b544485535c997cbcb866ada10c231c205fff3a
DutchJavaDev <borismulder@outlook.com>
2024-10-08 21:49:32 +0200

    test page generation

--- GithubWebpagesWebhook/Function1.cs
+++ GithubWebpagesWebhook/Function1.cs
@@ -1,25 +1,30 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace GithubWebpagesWebhook
 {
-    public static class Function1
+  public static class Function1
+  {
+    [FunctionName("GithubWebHook")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName("GithubWebHook")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Admin, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+      log.LogInformation("C# HTTP trigger function processed a request.");
 
-            return new OkObjectResult("GithubWebHook test");
-        }
+      var page = await File.ReadAllTextAsync("PageGenerator/index.html");
+      
+      return new ContentResult() 
+      {
+        Content = page,
+        ContentType = "document"
+      };
     }
+  }
 }
