08d96318d8f2519c359081985319cf7a0d388f98
DutchJavaDev <borismulder@outlook.com>
2024-10-09 20:11:58 +0200

    workaround but this should do the job

--- GithubWebpagesWebhook/Function1.cs
+++ GithubWebpagesWebhook/Function1.cs
@@ -4,13 +4,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
 using System;
-using System.IO;
-using System.Text;
-using Microsoft.AspNetCore.Mvc.Filters;
-using System.Threading;
-using ExecutionContext = System.Threading.ExecutionContext;
-using System.Reflection;
 
 namespace GithubWebpagesWebhook
 {
@@ -23,40 +18,16 @@ namespace GithubWebpagesWebhook
     {
       log.LogInformation("C# HTTP trigger function processed a request.");
 
-      //var page = await File.ReadAllTextAsync("PageGenerator/index.html");
+      var client = new BlobClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"),"html-templates","index.html");
 
-      try
+      var content = await client.DownloadContentAsync();
+
+      return new ContentResult() 
       {
-        var directories = new[] 
-        {
-          Environment.SystemDirectory,
-          Environment.CurrentDirectory,
-          Directory.GetCurrentDirectory(),
-          Directory.GetDirectoryRoot(Assembly.GetExecutingAssembly().Location)
-        };
-
-        var builder = new StringBuilder();
-
-        foreach (var directory in directories) 
-        {
-          if (string.IsNullOrEmpty(directory)) continue;
-
-          var subDirs = Directory.GetDirectories(directory);
-
-          var files = Directory.GetFiles(directory);
-
-          foreach (var file in files) 
-          {
-            builder.AppendLine(file);
-          }
-        }
-
-        return new OkObjectResult(builder.ToString());
-      }
-      catch (Exception e)
-      {
-        return new OkObjectResult(e.ToString());
-      }
+        Content = content.Value.Content.ToString(),
+        ContentType = "document",
+        StatusCode = 200,
+      };
     }
   }
 }
