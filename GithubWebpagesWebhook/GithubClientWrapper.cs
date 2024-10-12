7146424b7f8ae62e4e63f7839fc0976f298b1f48
DutchJavaDev <borismulder@outlook.com>
2024-10-12 22:54:45 +0200

    cloud test

--- GithubWebpagesWebhook/GithubClientWrapper.cs
+++ GithubWebpagesWebhook/GithubClientWrapper.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Octokit;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -61,25 +63,81 @@ namespace GithubWebpagesWebhook
     {
       try
       {
+        var client = new HttpClient();
         var repo = Environment.GetEnvironmentVariable("WebPagesRepositorieName");
-
+        var branch = "main";
         // Get the repository contents
-        var contents = await _client.Repository.Content.GetAllContentsByRef(_clientLogin, repo, "/", "main");
+        var contents = await _client.Repository.Content.GetAllContentsByRef(_clientLogin, repo, "/", branch);
 
-        var index = contents.First(i => i.Type == ContentType.File && i.Name.Equals("index.html"));
+        string indexPath = string.Empty;
 
-        //foreach (var content in contents)
-        //{
-        //  if (content.Type == ContentType.File)
-        //  {
-        //    var fileBytes = Convert.FromBase64String(content.EncodedContent);
-        //    File.WriteAllBytes(Path.Combine(LocalDirectory, content.Name), fileBytes);
-        //  }
-        //}
+        foreach (var content in contents)
+        {
+          if (content.Type == ContentType.File)
+          {
+            var fileBytes = await client.GetStringAsync(content.DownloadUrl);
+
+            var path = Path.Combine(LocalDirectory, content.Name);
+
+            File.WriteAllText(path, fileBytes);
+
+            if(content.Name.Contains("index.html"))
+            {
+              indexPath = path;
+            }
+          }
+        }
+
+        var blobClient = new BlobClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"), "html-templates", "index.html");
+
+        var blobContent = await blobClient.DownloadContentAsync();
+
+        var projects = await ProjectDivGenerator.GenerateProjectDivsAsync();
+
+        var htmlTemplate = blobContent.Value.Content.ToString()
+          .Replace("[user-name]", ClientLogin)
+          .Replace("[page-content]", projects)
+          .Replace("[last-update]", DateTime.Now.ToLongDateString());
+
+        File.WriteAllText(indexPath, htmlTemplate);
+
+        // Step 3a: Get the latest commit on the main branch
+        var baseRef = await _client.Git.Reference.Get(_clientLogin, repo, $"heads/{branch}");
+
+        // Step 3b: Prepare the new tree
+        var treeBuilder = new NewTree();
+        foreach (var file in Directory.GetFiles(LocalDirectory))
+        {
+          var fileName = Path.GetFileName(file);
+          var content = File.ReadAllText(file);
+
+          // Create a new tree entry for each file
+          treeBuilder.Tree.Add(new NewTreeItem
+          {
+            Path = fileName,
+            Mode = "100644",
+            Type = TreeType.Blob,
+            Content = content
+          });
+        }
+
+        // Create a new tree from the tree builder
+        var newTree = await _client.Git.Tree.Create(_clientLogin, repo, treeBuilder);
+
+        // Step 3c: Create a new commit
+        // Step 3c: Create a new commit
+        var changes = new NewCommit("Updated the repository with new changes", newTree.Sha, new[] { baseRef.Object.Sha })
+        {
+        };
+
+        var commit = await _client.Git.Commit.Create(_clientLogin, repo, changes);
+
+        // Step 3d: Update the main branch to point to the new commit
+        await _client.Git.Reference.Update(_clientLogin, repo, $"heads/{branch}", new ReferenceUpdate(commit.Sha));
 
         return new ContentResult() 
         {
-          Content = Encoding.UTF8.GetString(Convert.FromBase64String(index.EncodedContent)),
+          Content = "Okay",
           ContentType = "document",
           StatusCode = 200,
         };
