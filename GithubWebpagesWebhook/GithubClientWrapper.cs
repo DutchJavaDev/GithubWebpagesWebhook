6d6933ce0f70c96c9a68cfd8e43359425308bc1d
DutchJavaDev <borismulder@outlook.com>
2024-10-13 17:21:07 +0200

    more readable

--- GithubWebpagesWebhook/GithubClientWrapper.cs
+++ GithubWebpagesWebhook/GithubClientWrapper.cs
@@ -1,13 +1,7 @@
-﻿using Azure.Storage.Blobs;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Octokit;
+﻿using Octokit;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GithubWebpagesWebhook
@@ -15,8 +9,9 @@ namespace GithubWebpagesWebhook
   public static class GithubClientWrapper
   {
     private static readonly GitHubClient _client;
-    private static string _clientLogin;
-    public static string LocalDirectory = Path.Combine(Path.GetTempPath(), "my-cloned-repo");
+    private static readonly string _clientLogin;
+    private static readonly string _webPageRepositoryName = Environment.GetEnvironmentVariable("WebPagesRepositorieName");
+
     static GithubClientWrapper()
     {
       var accessToken = Environment.GetEnvironmentVariable("GithubAccessToken");
@@ -25,8 +20,6 @@ namespace GithubWebpagesWebhook
       {
         Credentials = credentials
       };
-      Directory.CreateDirectory(LocalDirectory);
-
       var user = _client.User.Current().Result;
       _clientLogin = user.Login;
     }
@@ -59,93 +52,31 @@ namespace GithubWebpagesWebhook
       return commitMessage;
     }
 
-    public static async Task<IActionResult> CloneTest()
+    public static async Task<IReadOnlyList<RepositoryContent>> GetAllRepositoryContentAsync(string branch = "main")
     {
-      try
-      {
-        var client = new HttpClient();
-        var repo = Environment.GetEnvironmentVariable("WebPagesRepositorieName");
-        var branch = "main";
-        // Get the repository contents
-        var contents = await _client.Repository.Content.GetAllContentsByRef(_clientLogin, repo, "/", branch);
+      return await _client.Repository.Content.GetAllContentsByRef(_clientLogin, _webPageRepositoryName, "/", branch);
+    }
 
-        string indexPath = string.Empty;
+    public static async Task<Reference> GetReferenceAsync(string branch = "main")
+    {
+      return await _client.Git.Reference.Get(_clientLogin, _webPageRepositoryName, $"heads/{branch}");
+    }
 
-        foreach (var content in contents)
-        {
-          if (content.Type == ContentType.File)
-          {
-            var fileBytes = await client.GetStringAsync(content.DownloadUrl);
+    public static async Task<TreeResponse> CreateTreeResponseAsync(NewTree treeBuilder)
+    {
+      return await _client.Git.Tree.Create(_clientLogin, _webPageRepositoryName, treeBuilder);
+    }
 
-            var path = Path.Combine(LocalDirectory, content.Name);
+    public static async Task<Commit> CreateCommitAsync(TreeResponse newTree, Reference baseRef)
+    {
+      var changes = new NewCommit("Updated the repository with new changes via GitHubWebpageWebHook", newTree.Sha, new[] { baseRef.Object.Sha });
 
-            File.WriteAllText(path, fileBytes);
+      return await _client.Git.Commit.Create(_clientLogin, _webPageRepositoryName, changes);
+    }
 
-            if(content.Name.Contains("index.html"))
-            {
-              indexPath = path;
-            }
-          }
-        }
-
-        var blobClient = new BlobClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"), "html-templates", "index.html");
-
-        var blobContent = await blobClient.DownloadContentAsync();
-
-        var projects = await ProjectDivGenerator.GenerateProjectDivsAsync();
-
-        var htmlTemplate = blobContent.Value.Content.ToString()
-          .Replace("[user-name]", ClientLogin)
-          .Replace("[page-content]", projects)
-          .Replace("[last-update]", DateTime.Now.ToLongDateString());
-
-        File.WriteAllText(indexPath, htmlTemplate);
-
-        // Step 3a: Get the latest commit on the main branch
-        var baseRef = await _client.Git.Reference.Get(_clientLogin, repo, $"heads/{branch}");
-
-        // Step 3b: Prepare the new tree
-        var treeBuilder = new NewTree();
-        foreach (var file in Directory.GetFiles(LocalDirectory))
-        {
-          var fileName = Path.GetFileName(file);
-          var content = File.ReadAllText(file);
-
-          // Create a new tree entry for each file
-          treeBuilder.Tree.Add(new NewTreeItem
-          {
-            Path = fileName,
-            Mode = "100644",
-            Type = TreeType.Blob,
-            Content = content
-          });
-        }
-
-        // Create a new tree from the tree builder
-        var newTree = await _client.Git.Tree.Create(_clientLogin, repo, treeBuilder);
-
-        // Step 3c: Create a new commit
-        // Step 3c: Create a new commit
-        var changes = new NewCommit("Updated the repository with new changes", newTree.Sha, new[] { baseRef.Object.Sha })
-        {
-        };
-
-        var commit = await _client.Git.Commit.Create(_clientLogin, repo, changes);
-
-        // Step 3d: Update the main branch to point to the new commit
-        await _client.Git.Reference.Update(_clientLogin, repo, $"heads/{branch}", new ReferenceUpdate(commit.Sha));
-
-        return new ContentResult() 
-        {
-          Content = "Okay",
-          ContentType = "document",
-          StatusCode = 200,
-        };
-      }
-      catch (Exception ex)
-      {
-        return new OkObjectResult(ex);
-      }
+    public static async Task UpdateRepositoryAsync(Commit commit, string branch = "main")
+    {
+      await _client.Git.Reference.Update(_clientLogin, _webPageRepositoryName, $"heads/{branch}", new ReferenceUpdate(commit.Sha));
     }
   }
 }
